311b62332ada70a2233fad739b1c2fb651cc43e2
Hesham <hesham.aly20@gmail.com>
2019-06-16 18:27:14 -0700

    add test7

--- kAPPot/Controller/ProgressionScreenVC.swift
+++ kAPPot/Controller/ProgressionScreenVC.swift
@@ -23,6 +23,7 @@ class ProgressionScreenVC: UIViewController{
         print("test3")
         print("tset4")
         print("test5")
+        print("test7")
             //navigationController!.navigationBar.shadowImage = UIImage()
         //navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .default)
         
